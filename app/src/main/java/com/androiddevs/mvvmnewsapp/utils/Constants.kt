6ab147701e9e33a55ff9f677cbe1695a5ff092a4
Mark-McCleane <mccleanemark@gmail.com>
2023-04-22 23:49:04 +0100

    setup retrofit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY = "59963ca0d3ad45d2beacd9c36633370e"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
