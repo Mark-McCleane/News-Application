dec346fe799403d84351423fc27c1fd1d208960c
Mark-McCleane <mccleanemark@gmail.com>
2023-04-24 00:20:09 +0100

    Adding and deleting articles to saved fragment

--- app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
@@ -3,6 +3,7 @@ package com.androiddevs.mvvmnewsapp.repository
 import androidx.room.Query
 import com.androiddevs.mvvmnewsapp.api.RetrofitInstance
 import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
+import com.androiddevs.mvvmnewsapp.model.Article
 
 class NewsRepository(val db: ArticleDatabase) {
     suspend fun getBreakingNews(countryCode:String, pageNumber: Int) =
@@ -10,4 +11,10 @@ class NewsRepository(val db: ArticleDatabase) {
 
     suspend fun searchNews(searchQuery: String, pageNumber: Int) =
         RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getSavedNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
 }
